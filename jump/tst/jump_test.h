48abd4108e765986a77abc94fc266f6aa4766b59
Jahrme Risner <jrisner@pugetsound.edu>
2018-02-08 01:48:38 -0800

    Updated Testing

---
+++ jump/tst/jump_test.h
@@ -0,0 +1,26 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <math.h>
+
+typedef struct distribution_stats {
+    double variance;
+    double standard_deviation;
+    double standard_error;
+} distribution_stats;
+
+uint64_t long_random ();
+
+void run_distrobution_tests
+    (int base, int min_power, int max_power, int step, int printing);
+
+distribution_stats test_distribution_random_keys
+    (int num_keys, int num_buckets);
+
+distribution_stats test_distribution_sequen_keys
+    (int num_keys, int num_buckets);
+
+distribution_stats calculate_distribution_stats
+    (int num_keys, int num_buckets, int bucket_items[]);
